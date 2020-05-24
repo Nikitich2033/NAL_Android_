d83b88072f87d0934dcc3bc71fb520cb2b0eaf0e
Russianbear8 <51367883+Russianbear8@users.noreply.github.com>
2020-05-24 21:40:02 +0100

    f

---
+++ app/src/main/java/makeAppointment/mappedSalons.java
@@ -0,0 +1,16 @@
+package makeAppointment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nal.R;
+
+public class mappedSalons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapped_salons);
+    }
+}
