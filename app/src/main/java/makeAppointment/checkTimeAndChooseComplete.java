500380ebbbf2f83f41e0908a2c13e32ac2bcc0a8
Russianbear8 <51367883+Russianbear8@users.noreply.github.com>
2020-05-24 16:43:58 +0100

    f

---
+++ app/src/main/java/makeAppointment/checkTimeAndChooseComplete.java
@@ -0,0 +1,16 @@
+package makeAppointment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nal.R;
+
+public class checkTimeAndChooseComplete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_time_and_choose_complete);
+    }
+}
