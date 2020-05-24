7d67f8d810252c03d6cdd6e59edd0553044ee1ba
Russianbear8 <51367883+Russianbear8@users.noreply.github.com>
2020-05-24 23:02:00 +0100

    f

--- app/src/main/java/makeAppointment/mappedSalons.java
+++ app/src/main/java/makeAppointment/mappedSalons.java
@@ -1,16 +1,57 @@
 package makeAppointment;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.example.nal.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class mappedSalons extends AppCompatActivity {
-
+public class mappedSalons extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap SalonsMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapped_salons);
+        ActionBar actionBar= getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.hide();
+        }
+       // SupportMapFragment mapFragment=(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+       // mapFragment.getMapAsync(mappedSalons.this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        SalonsMap=googleMap;
+        LatLng salon1=new LatLng(55.969712, -3.306804);
+        SalonsMap.addMarker(new MarkerOptions().position(salon1).title("My Salon")
+        .icon(bitmapDescriptor(getApplicationContext(),R.drawable.ic_flag_black_24dp)));
+
+        SalonsMap.moveCamera(CameraUpdateFactory.newLatLng(salon1));
+    }
+
+    private BitmapDescriptor bitmapDescriptor(Context context,int vectorResId){
+        Drawable vectorDrawable= ContextCompat.getDrawable(context,vectorResId);
+        vectorDrawable.setBounds(0,0,vectorDrawable.getIntrinsicWidth(),
+                vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap=Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(),
+                vectorDrawable.getIntrinsicHeight(),Bitmap.Config.ARGB_8888);
+        Canvas canvas=new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 }
