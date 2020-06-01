5c4380aa29f1698373a4e57fe834f2b635751362
Russianbear8 <51367883+Russianbear8@users.noreply.github.com>
2020-06-01 16:28:16 +0100

    j

--- app/src/main/java/com/example/nal/MainActivity.java
+++ app/src/main/java/com/example/nal/MainActivity.java
@@ -1,26 +1,21 @@
 package com.example.nal;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.ArrayList;
 
 import home.home;
-import makeAppointment.allFilteredSalonsByName;
 import makeAppointment.makeAppointmentByMainTags;
-import makeAppointment.rvAdapterFilteredSalons;
 import mySQLInteractions.sqlInteractions;
 
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,4 +23,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent=new Intent(this, home.class);
         startActivity(intent);
     }
+
 }
